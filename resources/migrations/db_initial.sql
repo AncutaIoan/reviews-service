50abb8f7b0e39e5a882a07d4ed68051112c51e33
eoancuta <eoancuta@gmail.com>
2025-03-03 10:09:06 +0200

    feat: removed comments, start working on timestamp created and updated at

--- resources/migrations/db_initial.sql
+++ resources/migrations/db_initial.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS reviews (
-                         id SERIAL PRIMARY KEY,            -- Auto-incremented ID for the review
-                         added_by VARCHAR(255) NOT NULL,    -- User who added the review
-                         added_at VARCHAR(255) NOT NULL,      -- Timestamp when the review was added
-                         rating INTEGER NOT NULL,          -- Rating value (e.g., 1-5)
-                         entity_type VARCHAR(255) NOT NULL, -- Type of entity being reviewed (e.g., 'Product', 'Person', etc.)
-                         entity_id VARCHAR(255) NOT NULL   -- ID of the entity being reviewed (e.g., product ID, person ID, etc.)
+                         id SERIAL PRIMARY KEY,
+                         added_by VARCHAR(255) NOT NULL,
+                         added_at VARCHAR(255) NOT NULL,
+                         rating INTEGER NOT NULL,
+                         entity_type VARCHAR(255) NOT NULL,
+                         entity_id VARCHAR(255) NOT NULL
 );
 
 
